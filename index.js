9275010e94c2a995d142f8c90208607682205e1a
aflah haami <aflahhaami@aflahs-MacBook-Air.local>
2025-02-06 12:23:52 +0530

    updated

---
+++ index.js
@@ -0,0 +1,28 @@
+
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// MongoDB Connection
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => {
+    console.log('Connected to database');
+})
+.catch((error) => {
+    console.error('Error connecting to database:', error);
+});
+
+
+app.get('/', (req, res) => {
+  res.send('Customer Management System Backend');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
